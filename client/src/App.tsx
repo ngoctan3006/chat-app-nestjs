93e81f401819e2e75259202d5352376a5bbfb799
Tan <>
2023-04-04 11:22:49 +0700

    route app with react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 const App: React.FC = () => {
-  return <div className="app">Hello</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route
+          path="/conversations"
+          element={
+            <div>
+              <div>Conversations</div>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path=":id" element={<div>Conversation ID Page</div>} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
